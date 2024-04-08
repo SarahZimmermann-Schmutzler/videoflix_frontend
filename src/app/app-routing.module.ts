edd3fde5c4e2965bc58e9ed8ff632f5f1307d2e5
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-04-08 10:34:43 +0200

    data protection everything but content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component:HomeComponent},
   {path: 'login', component:LoginComponent},
   {path: 'data', component:DataProtectionComponent},
 ];
