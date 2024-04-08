6543036452d10196af217200d53a1b084e17d110
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-04-08 11:28:21 +0200

    legal notice ready without content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { HomeComponent } from './home/home.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component:HomeComponent},
   {path: 'login', component:LoginComponent},
   {path: 'data', component:DataProtectionComponent},
+  {path: 'legal', component:LegalNoticeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { HomeComponent } from './home/home.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DataProtectionComponent,
-    HomeComponent
+    HomeComponent,
+    LegalNoticeComponent
   ],
   imports: [
     BrowserModule,
