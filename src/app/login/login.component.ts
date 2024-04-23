8a14776f323d1e405679ef75104d257ecc173aba
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-04-23 11:13:04 +0200

    login changeBorderColor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,10 +10,20 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   username = '';
   password = '';
+  email = '';
+  redMailBorder = false;
+  redPwdBorder = false;
+  disabledButton = true;
 
   constructor(private authservice: AuthService, private router: Router) { }
 
+  ngOnInit() {
+    this.watchForm();
+  }
+
   async login() {
+    let partBeforeAt = this.email.split('@');
+    this.username = partBeforeAt[0];
     try {
       let resp = await this.authservice.loginWithUserAndPassword(this.username, this.password);
       localStorage.setItem('token', resp['token'])
@@ -24,6 +34,35 @@ export class LoginComponent {
     } catch (e) {
       console.error(e);
       // window.location.reload();
+    }
+  }
+
+  watchForm() {
+    setInterval(() => {
+      if (this.redMailBorder == false && this.redPwdBorder == false) {
+        this.disabledButton = false;
+      } else {
+        this.disabledButton = true;
+      }
+    }, 500);
+  }
+
+  changeBorderColor(event) {
+    if (event.isTrusted == true) {
+      this.redMailBorder = true;
+      this.redPwdBorder = true;
+    }
+
+    if (this.email.includes("@") && this.email.includes(".")) {
+      this.redMailBorder = false;
+    } else {
+      this.redMailBorder = true;
+    }
+
+    if (this.password.length > 7) {
+      this.redPwdBorder = false;
+    } else {
+      this.redPwdBorder = true;
     }
   }
 }
