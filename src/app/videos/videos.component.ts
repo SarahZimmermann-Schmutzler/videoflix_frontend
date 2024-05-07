d36a9b3308ec8eee2fdf7492baa13f2022a95afe
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-05-07 15:30:55 +0200

    show and hide slide elements

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,8 +14,11 @@ export class VideosComponent {
   slide_right = false;
   slide_right_stay = false;
   slide_left = false;
+  also_left_slide = false;
   slide_left_stay = false;
- 
+  currentIndex = 0;
+  slideWidth: number;
+
   dummyArray = [
     {
       'title': 'Coming Soon',
@@ -81,30 +84,41 @@ export class VideosComponent {
       'title': 'Coming Soon',
       'img': 'quelle'
     },
-    
+
   ];
 
-  constructor(public router: Router) {}
+  @ViewChild('carousel') carousel: ElementRef;
+
+  constructor(public router: Router) { }
 
   ngOnInit() {
-
+    this.slideWidth = 230; // Breite eines einzelnen Slides in Pixel
+    this.dummyArray.push(this.dummyArray[0]);
   }
 
-  scroll(el: HTMLElement){
+  scroll(el: HTMLElement) {
     el.scrollIntoView
   }
 
   hideLogoutMenu() {
-      setTimeout(() => {
+    setTimeout(() => {
       this.showMenu = false;
     }, 1000)
   }
 
   hideSlider() {
     setTimeout(() => {
-    this.slide_right = false;
-  }, 500)
-}
+      this.slide_right = false;
+      this.slide_left = false;
+    }, 500)
+  }
+
+  showSlider() {
+    this.slide_right = true;
+    if(this.also_left_slide == true) {
+      this.slide_left = true;
+    }
+  }
 
   logout() {
     this.router.navigateByUrl('/').then(() => {
@@ -113,7 +127,37 @@ export class VideosComponent {
   }
 
   slideRight() {
+    this.also_left_slide = true;
     this.slide_left = true;
+
+    if (this.currentIndex === this.dummyArray.length - 1) {
+      this.currentIndex = 0;
+      setTimeout(() => {
+        this.carousel.nativeElement.style.transition = 'none';
+        this.carousel.nativeElement.style.transform = 'translateX(0)';
+        setTimeout(() => {
+          this.carousel.nativeElement.style.transition = '';
+        });
+      }, 50);
+    } else {
+      this.currentIndex++;
+    }
+  }
+  
+
+  slideLeft() {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.dummyArray.length - 1;
+      setTimeout(() => {
+        this.carousel.nativeElement.style.transition = 'none';
+        this.carousel.nativeElement.style.transform = `translateX(-${this.slideWidth * (this.dummyArray.length - 1)}px)`;
+        setTimeout(() => {
+          this.carousel.nativeElement.style.transition = '';
+        });
+      }, 50);
+    } else {
+      this.currentIndex--;
+    }
   }
 }
 
