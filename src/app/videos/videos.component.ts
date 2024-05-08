0b8cd8f4583f263377701b907c399d0df3c3576f
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-05-08 12:23:35 +0200

    try to slide left infinite

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -17,66 +17,43 @@ export class VideosComponent {
   also_left_slide = false;
   slide_left_stay = false;
   currentIndex = 0;
-  slideWidth: number;
+  preview_element_width = 230;
+  nextIndex = 0;
+  previousIndex: number;
 
   dummyArray = [
     {
-      'title': 'Coming Soon',
+      'title': 'Coming Soon1',
       'img': 'quelle'
     },
 
     {
-      'title': 'Coming Soon',
+      'title': 'Coming Soon2',
       'img': 'quelle'
     },
 
     {
-      'title': 'Coming Soon',
+      'title': 'Coming Soon3',
       'img': 'quelle'
     },
 
     {
-      'title': 'Coming Soon',
+      'title': 'Coming Soon4',
       'img': 'quelle'
     },
 
     {
-      'title': 'Coming Soon',
+      'title': 'Coming Soon5',
       'img': 'quelle'
     },
 
     {
-      'title': 'Coming Soon',
+      'title': 'Coming Soon6',
       'img': 'quelle'
     },
 
     {
-      'title': 'Coming Soon',
-      'img': 'quelle'
-    },
-
-    {
-      'title': 'Coming Soon',
-      'img': 'quelle'
-    },
-
-    {
-      'title': 'Coming Soon',
-      'img': 'quelle'
-    },
-
-    {
-      'title': 'Coming Soon',
-      'img': 'quelle'
-    },
-
-    {
-      'title': 'Coming Soon',
-      'img': 'quelle'
-    },
-
-    {
-      'title': 'Coming Soon',
+      'title': 'Coming Soon7',
       'img': 'quelle'
     },
 
@@ -92,8 +69,8 @@ export class VideosComponent {
   constructor(public router: Router) { }
 
   ngOnInit() {
-    this.slideWidth = 230; // Breite eines einzelnen Slides in Pixel
-    this.dummyArray.push(this.dummyArray[0]);
+    // this.slideWidth = 230; // Breite eines einzelnen Slides in Pixel
+    // this.dummyArray.push(this.dummyArray[0]);
   }
 
   scroll(el: HTMLElement) {
@@ -115,8 +92,14 @@ export class VideosComponent {
 
   showSlider() {
     this.slide_right = true;
-    if(this.also_left_slide == true) {
+    if (this.also_left_slide == true) {
       this.slide_left = true;
+    }
+  }
+
+  showLeftSlide() {
+    if (this.also_left_slide == true) {
+      this.slide_left_stay = true;
     }
   }
 
@@ -130,32 +113,64 @@ export class VideosComponent {
     this.also_left_slide = true;
     this.slide_left = true;
 
+    if (this.dummyArray.length > 0) {
+      // wenn Array mind. 1 Object
+      let nextElement = this.dummyArray[this.nextIndex];
+      // nextElement ist Array an Stelle 0;
+      this.dummyArray.push(nextElement);
+      // Array an Stelle 0 (=erstes Element) wird in das Array gepusht
+      this.nextIndex = (this.nextIndex + 1) % this.dummyArray.length;
+      // next Index wird um 1 vermindert bis Array zuende ist
+      console.log(this.dummyArray.length)
+    }
+
     if (this.currentIndex === this.dummyArray.length - 1) {
       this.currentIndex = 0;
-      setTimeout(() => {
-        this.carousel.nativeElement.style.transition = 'none';
-        console.log(this.carousel.nativeElement.style.transition)
-        this.carousel.nativeElement.style.transform = 'translateX(0)';
-        setTimeout(() => {
-          this.carousel.nativeElement.style.transition = '';
-        });
-      }, 50);
     } else {
       this.currentIndex++;
     }
   }
-  
+
 
   slideLeft() {
+
+    // if (this.dummyArray.length > 0) {
+    //   // wenn Array mind. 1 Object
+    //   this.previousIndex = this.dummyArray.length
+    //   let previousElement = this.dummyArray[this.previousIndex];
+    //   // prevoiusElement ist Array an letzter Stelle;
+    //   this.dummyArray.push(previousElement);
+    //   // Array an letzter Stelle 0 (=letztes Element) wird in das Array gepusht
+    //   this.previousIndex = (this.nextIndex - 1) % this.dummyArray.length;
+    //   // // next Index wird erhöht um 1 bis Array zuende ist
+    //   // console.log(this.dummyArray.length)
+    // }
+
+    // if (this.dummyArray.length > 0) {
+    //   let lastElement = this.dummyArray[this.dummyArray.length - 1];
+    //   // das letzte Element des Arrays erhalten
+    //   this.dummyArray.push(lastElement);
+    //   // das letzte Element ans Ende des Arrays hinzufügen
+    //   console.log(this.dummyArray.length);
+    // }
+
+  //   let currentIndex = 0;
+
+  //   if (this.dummyArray.length > 0) {
+  //     let index = this.dummyArray.length - 1 - currentIndex;
+  //     if (index >= 0) {
+  //         let previousElement = this.dummyArray[index];
+  //         this.dummyArray.unshift(previousElement); // unshift() fügt das Element am Anfang des Arrays hinzu
+  //         console.log(this.dummyArray.length);
+  //         currentIndex++;
+  //     } else {
+  //         console.log("All objects added, starting from the last object again");
+  //         currentIndex = 0; // Index zurücksetzen, um wieder von vorne zu beginnen
+  //     }
+  // }
+
     if (this.currentIndex === 0) {
       this.currentIndex = this.dummyArray.length - 1;
-      setTimeout(() => {
-        this.carousel.nativeElement.style.transition = 'none';
-        this.carousel.nativeElement.style.transform = `translateX(-${this.slideWidth * (this.dummyArray.length - 1)}px)`;
-        setTimeout(() => {
-          this.carousel.nativeElement.style.transition = '';
-        });
-      }, 50);
     } else {
       this.currentIndex--;
     }
