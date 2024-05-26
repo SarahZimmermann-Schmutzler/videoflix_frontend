da4aa8b8719316172a7007db74b317de7c8e863a
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-05-26 11:14:44 +0200

    right baseurls

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
     baseURL: 'http://localhost:8000',
-    // baseURL:'https://backend.s-zimmermann-schmutzler.de/videoflix_backend',
 };
 
 // Entwicklungsmodus (mein Server)
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    // baseURL: 'http://localhost:8000',
     baseURL:'https://backend.s-zimmermann-schmutzler.de/videoflix_backend',
 };
 
