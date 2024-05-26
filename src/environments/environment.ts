649560ec2bd31db951ee96209b431ccebd159386
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-05-26 10:54:40 +0200

    test baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     // baseURL: 'http://localhost:8000',
-    baseURL:'https://backend.s-zimmermann-schmutzler.de/videoflix',
+    baseURL:'https://backend.s-zimmermann-schmutzler.de/videoflix_backend',
 };
 
 // Veröffentlichungsmodus; wird bei ng-build-prozess eingefügt 
